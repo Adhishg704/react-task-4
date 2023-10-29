b4a3b259f8dbd982c59d728c821b0d35e7f9c3a1
AD <adhish.g704@gmail.com>
2023-10-29 18:04:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserTable from './components/dummy';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
